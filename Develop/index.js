7d71bd77b4979c2252c15f1d5a52ef16e04fd148
egodschalk <egodschalk@gmail.com>
2024-07-25 02:06:01 -0400

    updates to readme sections

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,13 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
 const questions = [
 
-    inquirer
-        .prompt([
+    
             {
                 type: "input",
                 message: "What is your project title?",
@@ -22,23 +23,62 @@ const questions = [
                 message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running?",
                 name: "installation",
             },
-        ])
-        .then((answers) => {
-            console.log(answers)
-
-        })
-           
-    ]
+            {
+                type: "input",
+                message: "Provide instructions and examples for use. Include screenshots as needed.",
+                name: "usage",
+            },
+            {
+                type: "input",
+                message: "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.",
+                name: "credits",
+            },
+            {
+                type: "list",
+                message: "The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).",
+                name: "license",
+                choices: ["Apache License 2.0", "GNU GPLv2", "GNU GPLv3", "ISC License", "MIT License"],
+            },
+            {
+                type: "input",
+                message: "If your project has a lot of features, list them here.",
+                name: "features",
+            },
+            {
+                type: "input",
+                message: "If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.",
+                name: "contributing",
+            },
+            {
+                type: "input",
+                message: "Go the extra mile and write tests for your application. Then provide examples on how to run them here.",
+                name: "tests",
+            },
+            {
+                type: "input",
+                message: "Enter your GitHub username so users can contact you with questions",
+                name: "github",
+            },
+            {
+                type: "input",
+                message: "Enter your email so users can contact you with questions",
+                name: "email",
+            },
+        ]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile('README.md', questions, (err) =>
+    fs.writeFile('README.md', generateMarkdown(data), (err) =>
         err ? console.error(err) : console.log('Success!')
       );
  }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() { 
+    inquirer.prompt(questions).then(data => {
+        writeToFile('README.md', data)
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,6 +14,65 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${data.badges}
+
+  ## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+- [Features](#features)
+- [How to Contribute](#how to contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## License
+
+${data.license}
+
+---
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+${data.features}
+
+## How to Contribute
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+[GitHub Profile](https://github.com/${data.github})
+[egodschalk@gmail.com](mailto:${data.email})
+
 `;
 }
 
